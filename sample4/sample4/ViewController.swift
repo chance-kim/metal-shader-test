8f9a8640d1e5ff4e219c500636beb2e41aba95cc
chance.k <chance.k@kakaoenterprise.com>
2021-02-03 23:40:32 +0900

    샘플4 추가 : 블러 작업용

--- sample4/sample4/ViewController.swift
+++ sample4/sample4/ViewController.swift
@@ -6,14 +6,22 @@
 //
 
 import UIKit
+import MetalKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mtkView: MTKView!
+    
+    var renderer:Renderer = .init()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.mtkView.enableSetNeedsDisplay = true
+        self.mtkView.device = self.renderer.device
+        self.mtkView.delegate = self.renderer
     }
-
 
 }
 
